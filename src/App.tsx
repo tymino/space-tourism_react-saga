fc02db70a272e3205fdc86e4595bfd9793b7d4c5
tymino <d_team-lex@bk.ru>
2021-10-22 14:41:51 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './sass/app.sass';
+
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      React Typescript
+    </div>
+  );
+}
+
+export default App;
