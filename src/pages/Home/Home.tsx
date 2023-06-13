ee871322125b9ec0f219f6c1a7c0f041cab41d1d
Aleksey <d_team-lex@bk.ru>
2023-06-13 14:11:31 +0300

    refac: Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -31,19 +31,17 @@ const Home = () => {
       />
 
       <div className="home__container">
-        <div className="home__container-describe">
-          <h2 className="home__container-describe-subtitle">
-            So, you want to travel to
-          </h2>
-          <h1 className="home__container-describe-header">Space</h1>
-          <div className="home__container-describe-text">
+        <div className="home__describe">
+          <h2 className="home__describe-subtitle">So, you want to travel to</h2>
+          <h1 className="home__describe-header">Space</h1>
+          <div className="home__describe-text">
             Let's face it; if you want to go to space, you might as well
             genuinely go to outer space and not hover kind of on the edge of it.
             Well sit back, and relax because we'll give you a truly out of this
             world experience!
           </div>
         </div>
-        <div className="home__container-explore">Explore</div>
+        <div className="home__explore">Explore</div>
       </div>
     </div>
   );
