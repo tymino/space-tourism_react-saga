535e03abd5219e505f18d280718d797bddc5f382
Aleksey <d_team-lex@bk.ru>
2023-06-22 17:23:10 +0300

    style: Loading component

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,19 +1,36 @@
-// import './Loading.sass';
-import { FC } from 'react';
 import { useSelector } from 'react-redux';
 import { selectLoading } from '../../redux/store';
+
+import { styled } from 'styled-components';
+import { heading4 } from '../../styles/mixins/heading';
+import { device } from '../../styles/mediaSize';
+
+const LoadingContainer = styled.div`
+  margin-top: 40px;
+  margin-left: 55px;
+  color: ${({ theme }) => theme.colorDescribeText};
+  ${heading4};
+
+  @media ${device.laptop} {
+    margin-left: 40px;
+  }
+
+  @media ${device.mobileL} {
+    margin-left: 24px;
+  }
+`;
 
 interface ILoadingProps {
   children: JSX.Element;
 }
 
-export const Loading: FC<ILoadingProps> = ({ children }) => {
+export const Loading = ({ children }: ILoadingProps) => {
   const isLoading = useSelector(selectLoading);
 
   return (
     <div>
       {isLoading ? (
-        <div className="loading">Loading...</div>
+        <LoadingContainer>Loading...</LoadingContainer>
       ) : (
         <div>{children}</div>
       )}
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -16,3 +16,4 @@ export const theme = {
   colorWhite -> colorText
   colorLight -> colorDescribeText,
 */
+
