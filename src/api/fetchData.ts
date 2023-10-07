1890e2e7c37612d693751af7110901f3bf055146
Aleksey <d_team-lex@bk.ru>
2023-10-07 19:23:28 +0300

    delete c.log

--- src/api/fetchData.ts
+++ src/api/fetchData.ts
@@ -2,7 +2,5 @@ export const fetchData = async (pageName: string) => {
   const response = await fetch(`${process.env.REACT_APP_API_PATH}/${pageName}`);
   const json = await response.json();
 
-  console.log(json);
-
   return json;
 };
